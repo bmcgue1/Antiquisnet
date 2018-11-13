a1f906faaf28b28f7b3f463bf8ecfae0bae50d30
mike2flee <Micjj78@gmail.com>
2018-11-13 17:02:59 -0500

    changed login.php to fit the new databse table and started a signup html

--- login.php
+++ login.php
@@ -7,13 +7,13 @@ include 'dbconfig.php';
 //sets up the connection parammeters 
 $con = mysqli_connect($host, $username, $password,$dbname) or die(mysql_error());
 //the query to authorise the
-$query = "SELECT * FROM Creds WHERE login = '$login'";
+$query = "SELECT * FROM Credentials WHERE username = '$login'";
 $results = mysqli_query($con, $query);
 
 
 echo "<br>".$query ."<br>";
 while($row = mysqli_fetch_array($results)) {
-  $the_user = $row["login"];
+  $the_user = $row["username"];
   }
   echo "The users name is  $the_user ";
 
