5126735a5b7fde0d10d8df835d8fff329152b9ad
mike2flee <Micjj78@gmail.com>
2018-11-13 18:15:27 -0500

    confirms sign up and spits quey back out

--- signUp.php
+++ signUp.php
@@ -3,7 +3,7 @@ $logUsername = $_POST['username'];
 $fName = $_POST['fName'];
 $lName = $_POST['lName'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$logpassword = $_POST['password'];
 $cpassword = $_POST['cpassword'];
 
 include 'dbconfig.php';
@@ -12,9 +12,12 @@ include 'dbconfig.php';
 $con = mysqli_connect($host, $username, $password,$dbname) or die(mysql_error());
 //the query to authorise the
 
-$query = "INSERT INTO `antiquis_testRepo`.`Credentials` (`username`, `password`, `fName`, `lName`, `email`) VALUES ('$logUsername', '$password', '$fName', '$lName', '$email')";
-if($password == $cpassword){
+$query = "INSERT INTO `antiquis_testRepo`.`Credentials` (`username`, `password`, `fName`, `lName`, `email`) VALUES ('$logUsername', '$logpassword', '$fName', '$lName', '$email')";
+
+if($logpassword == $cpassword){
+    echo "sign up success";
     mysqli_query($con, $query);
+    echo "<br>".$query ."<br>";
 }
 
 ?>
